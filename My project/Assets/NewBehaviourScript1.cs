22a5f3abd1b220bbf0cd9fb0a42b5d77d2d01d10
junhae01 <jh040101@g.shingu.ac.kr>
2023-03-20 12:23:58 +0900

    변수선업

---
+++ My project/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string str1 = "happy";    //str ing 형 str1 선언 후 happy 값 입력
+        string str2 = "birthday";  //str ing 형 str2 선언 후 bir thday 값 입력
+        string message;
+
+        message = str1 + str2;
+        Debug.Log(message);
+
+        str1 += str2;
+        Debug.Log(str1);
+
+        int num = 123;
+        message = str1 + num;
+        Debug.Log(message);
+    }    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
